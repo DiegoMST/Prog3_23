4fd3ba137b7f427d8f98f3a64a5c588ce3d3e3f5
Diego Morais de Souza Soares <387510@unoesc.lan>
2023-03-06 22:09:06 -0300

    Exercicio 2

---
+++ Exe02/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("olá, seja bem-vindo!");
+Console.WriteLine("Qual é o seu nome?");
+string name = Console.ReadLine();
+Console.WriteLine($"olá {name} prazer conhece-lo");
+
+
+// Declarar sem inicializar / primitivo
+string message1;
+// Declarar inicializar / abstrato
+String messege2 = "Seu Nome Completo";
+// Declarar String vazia
+string message3 = String.Empty;
+string message4 = ""; 
+
+//Declaração com tipo implícito
+var message5 = "Mensagem aleatória";
+
+message1 = "Rogério Ceni;Pelé";
+var palavras = message1.Split(';');
+foreach(var word in palavras)
+{
+    //variável de escopo lical do
+    //laço de repetição foreach
+    int a = 10;
+    Console.WriteLine(word); 
+}
+
+
+
+
